3bb2a1a69acdb67f04e63dbea662885f2fc6c642
StickMouille <59177908+StickMouille@users.noreply.github.com>
2023-03-14 16:10:47 +0100

    Models

--- Models/Model.php
+++ Models/Model.php
@@ -43,6 +43,61 @@ class Model extends Db
         return $this->requete("SELECT * FROM {$this->table} WHERE id = ?", [$id])->fetch();
     }
 
+//CREATE
+    public function create(Model $model)
+    {
+        //Insert into offers (entitled, duration, date_publish , salary, space_available, state, description) values (?, ?, ?, ?, ?, ?, )?
+        $champs = [];
+        $inter = [];
+        $valeurs = [];
+
+    // On boucle pour "éclater le tableau"
+    foreach($model as $champ => $valeur)
+    {
+        if($valeur !== null && $champ != 'table' && $champ != 'db'){
+        $champs[] = $champ;
+        $inter[] = "?";
+        $valeurs[]= $valeur;
+        }
+    }
+    $liste_champs = implode(', ', $champs);
+    $liste_inter = implode(', ', $inter);
+
+    
+    // On exécute la requête
+    return $this->requete("INSERT INTO {$this->table} ($liste_champs) VALUES ($liste_inter)", $valeurs);
+    }
+
+//UPDATE
+    public function update(int $id, Model $model)
+    {
+        //update offers set entitled = ?, duration = ?, date_publish = ?, salary = ?, space_available = ?, state = ?, description = ? where id = ?
+        $champs = [];
+        $valeurs = [];
+
+    // On boucle pour "éclater le tableau"
+    foreach($model as $champ => $valeur)
+    {
+        if($valeur !== null && $champ != 'table' && $champ != 'db'){
+        $champs[] = "$champ = ?";
+        $valeurs[]= $valeur;
+        }
+    }
+    $valeurs[] = $id;
+    $liste_champs = implode(', ', $champs);
+
+    
+    // On exécute la requête
+    return $this->requete("UPDATE {$this->table} SET $liste_champs WHERE id = ?", $valeurs );
+    } 
+
+//DELETE
+    public function delete(int $id)
+    {
+    // DELETE FROM offers WHERE id = ?
+    return $this->requete("DELETE FROM {$this->table} WHERE id = ?", [$id]);
+    }
+    
 
     /**
     * Méthode qui exécutera les requêtes
@@ -66,4 +121,23 @@ class Model extends Db
         return $this->db->query($sql);
         }
     }
+
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            // On récupère le nom du setter correspondant à la clé
+            $setter = 'set'.ucfirst($key);
+
+            // Si le setter correspondant existe
+            if (method_exists($this, $setter))
+            {
+                // On appelle le setter
+                $this->$setter($value);
+            }
+        }
+        return $this;
+    }
+
 }
--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php 
 use App\Autoloader;
 use App\Models\OffersModel;
+use App\Models\UsersModel;
 
 require_once 'Autoloader.php';
 Autoloader::register();
 
 echo '<h1>page menu</h1>';
 
-$model = new OffersModel();
-$annonces = $model->find(1);
-var_dump($annonces);
+$user = $model = new UsersModel;
+$model->setEmail('koikoubeh@gmail.com')
+        ->setPassword(password_hash('0000', PASSWORD_ARGON2I));
+$model ->create($model);
+
 
 
 

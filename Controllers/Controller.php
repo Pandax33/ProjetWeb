4112e8ef62bf7820f7c1a8f11fb20353d816703d
StickMouille <59177908+StickMouille@users.noreply.github.com>
2023-03-20 11:54:35 +0100

    Ajout Bootstrap navbar + moteur de template

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -9,7 +9,14 @@ abstract class Controller
         // On extrait les données du tableau $data
         extract($data);
 
+        // On démarre le buffer de sortie
+        ob_start();
+        // A partir de ce point, tout est stocké en mémoire
+       
         require_once ROOT.'/Views/'.$fichier.'.php';
+        $contenu = ob_get_clean();
+
+        require_once ROOT.'/Views/default.php';
 
 
      }

5fd715a6670cad1e4a35f16f652540280f87e057
StickMouille <59177908+StickMouille@users.noreply.github.com>
2023-03-20 15:39:40 +0100

    Controller

---
+++ Controllers/EnterpriseController.php
@@ -0,0 +1,31 @@
+<?php 
+// PERMET DE CONSULTER LA LISTE DES Entreprises
+namespace App\Controllers;
+use App\Models\EnterpriseModel;
+
+class EnterpriseController extends Controller
+{
+    //lister les entreprises de la bdd
+    public function index()
+    {
+        //On instancie le modele coresspondant a la table enterprises
+        $enterpriseModel = new EnterpriseModel;
+
+        //On recupere les entreprises de la bdd 
+        $enterprises = $enterpriseModel->findAll();
+        $this -> render('enterprises/index', ['enterprises' => $enterprises]);
+
+    }
+
+    // Afficher une annonce
+    public function detail(int $id){
+        // On instancie le modèle
+        $enterpriseModel = new EnterpriseModel;
+
+        // On récupère l'entreprise
+        $enterprise = $enterpriseModel->find($id);
+
+        // On affiche la vue
+        $this->render('enterprises/detail', ['enterprise' => $enterprise]);
+    }
+}
---
+++ Controllers/StudentController.php
@@ -0,0 +1,35 @@
+<?php 
+// PERMET DE CONSULTER LA LISTE DES ÉTUDIANTS
+namespace App\Controllers;
+use App\Models\PersonModel;
+
+class StudentController extends Controller
+{
+    //lister les étudiants de la bdd
+    public function index()
+    {
+        //On instancie le modèle correspondant à la table students
+        $studentModel = new PersonModel;
+
+        //On récupère les étudiants de la bdd 
+        $students = $studentModel->findBy(['Role_P' => 'Student']);
+        $this -> render('students/index', ['students' => $students]);
+
+    }
+
+    // Afficher un étudiant
+    public function detail(int $id){
+        // On instancie le modèle
+        $studentModel = new PersonModel;
+
+        // On récupère l'étudiant
+        $student = $studentModel->find($id);
+
+        // On affiche la vue
+        $this->render('students/detail', ['student' => $student]);
+    }
+}
+
+
+
+

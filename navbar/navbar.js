ca9d1da7730cba849a5930db94f4d23997b7aea6
ibraa14 <ibrahim.ferkh@net.usj.edu.lb>
2023-03-22 10:43:12 +0100

    navbar-half finished

---
+++ navbar/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navList = document.getElementById('navList');
+  
+    hamburger.addEventListener('click', () => {
+      navList.classList.toggle('nav-list-active');
+    });
+  });
+  
